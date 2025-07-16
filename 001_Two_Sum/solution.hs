f9a6eff1b5f7c5380dc03b11c4aed52c408ce347
Yi-Ping Pan <pan.yiping.fi@gmail.com>
2025-07-16 16:38:06 +0800

    Add haskell solution

---
+++ 001_Two_Sum/solution.hs
@@ -0,0 +1,22 @@
+-- Leetcode 1. Two Sum
+
+import qualified Data.Map as Map
+
+-- 1. Iterate all items in list
+-- 2. Look up complement in map
+-- 3. If not exsists add to map
+
+twoSum :: [Int] -> Int -> [Int]
+twoSum nums target = go nums 0 Map.empty
+  where
+    go [] _ _ = []  -- not found
+    go (x:xs) i m =
+      case Map.lookup (target - x) m of
+        Just j  -> [j, i]  -- found the complement
+        Nothing -> go xs (i + 1) (Map.insert x i m)
+
+main :: IO ()
+main = do
+  print $ twoSum [2, 7, 11, 15] 9       -- [0,1]
+  print $ twoSum [3, 2, 4] 6            -- [1,2]
+  print $ twoSum [3, 3] 6               -- [0,1]
