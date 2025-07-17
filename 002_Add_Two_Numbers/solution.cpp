c1835e5644060c82f1f2a7b861250e19961b1c6a
Yi-Ping Pan <pan.yiping.fi@gmail.com>
2025-07-17 21:56:09 +0800

    Add 002_Add_Two_Numbers cpp solution

---
+++ 002_Add_Two_Numbers/solution.cpp
@@ -0,0 +1,39 @@
+// Leetcode 2. Add Two Numbers
+//
+// NOTE:
+// 1. Practice indirect pointer usage
+// 2. Handle same patten together
+
+// Definition for singly-linked list.
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+  ListNode *addTwoNumbers(ListNode *l1, ListNode *l2) {
+    ListNode *dummy = new ListNode();
+    ListNode **indirect = &dummy->next;
+    int c = 0; // carry
+
+    while (l1 || l2 || c) {
+      int a = l1 ? l1->val : 0;
+      int b = l2 ? l2->val : 0;
+
+      int sum = (a + b + c);
+      c = (a + b + c) / 10;
+      (*indirect) = new ListNode(sum % 10);
+
+      indirect = &(*indirect)->next;
+      if (l1)
+        l1 = l1->next;
+      if (l2)
+        l2 = l2->next;
+    }
+    return dummy->next;
+  }
+};
