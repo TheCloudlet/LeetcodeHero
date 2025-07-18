f80022baa02f10e10896498e11bdcca051c58f99
Yi-Ping Pan <pan.yiping.fi@gmail.com>
2025-07-18 21:05:20 +0800

    -

--- 002_Add_Two_Numbers/LinkedList.hs
+++ 002_Add_Two_Numbers/LinkedList.hs
@@ -4,14 +4,14 @@ module LinkedList
   , toStdList
   ) where
 
-data LinkedList a = LNil | LCons a (LinkedList a) deriving (Show, eq)
+data LinkedList a = LNil | LCons a (LinkedList a) deriving Show
 
 instance Foldable LinkedList where
   foldr _ z LNil = z
   foldr f z (LCons x xs) = f x (foldr f z xs)
 
 fromStdList :: [a] -> LinkedList a
-fromStdList = foldr LCons a LNil
+fromStdList = foldr LCons LNil
 
 toStdList :: LinkedList a -> [a]
 toStdList = foldr (:) []
