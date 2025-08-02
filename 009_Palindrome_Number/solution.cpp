969206edc3255abdde4f479f2eb42b93450e34a1
Yi-Ping Pan <pan.yiping.fi@gmail.com>
2025-08-02 13:14:12 +0800

    Add 009_Palindrome_Number cpp solution

---
+++ 009_Palindrome_Number/solution.cpp
@@ -0,0 +1,18 @@
+// Leetcode 9. Palindrome Number
+
+#include <string>
+class Solution {
+public:
+  bool isPalindrome(int x) {
+    if (x < 0) {
+      return false;
+    }
+    std::string s = std::to_string(x);
+    for (int l = 0, r = s.length() - 1; l < r; ++l, --r) {
+      if (s[l] != s[r]) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
