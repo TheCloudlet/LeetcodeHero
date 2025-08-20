f9ed96ee7208107b381b1e29232cb68a41132234
Yi-Ping Pan <pan.yiping.fi@gmail.com>
2025-08-20 17:42:25 +0800

    Add explaination between two solutions

--- 017_Letter_Combinations_of_a_Phone_Number/solution.cpp
+++ 017_Letter_Combinations_of_a_Phone_Number/solution.cpp
@@ -1,6 +1,17 @@
 // Leetcode: 17. Letter Combinations of a Phone Number
 // @tag: hash-table, string, backtracking
 // @difficulty: medium
+//
+// NOTE:
+//
+// This iterative solution is essentially a left fold (`foldl` in Haskell).
+// It starts with an initial value (a list containing an empty string) and
+// repeatedly applies a combination function for each digit.
+//
+// The canonical solution, however, uses backtracking. This is more akin to
+// the way a recursive list comprehension or a monadic `do` block in Haskell
+// would solve it, by defining how to combine one character with the result
+// of the recursive call on the rest of the digits.
 
 #include <string>
 #include <unordered_map>
