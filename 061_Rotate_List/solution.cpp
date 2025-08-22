1793edaeba5fb5b740ed81385a150943e90afcd9
Yi-Ping Pan <pan.yiping.fi@gmail.com>
2025-08-22 08:03:24 +0800

    Add 061_Rotate_List cpp solution

---
+++ 061_Rotate_List/solution.cpp
@@ -0,0 +1,45 @@
+// Leetcode 61. Rotate List
+// @tag: linked-list
+// @difficulty: medium
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+  ListNode *rotateRight(ListNode *head, int k) {
+    if (!head) {
+      return nullptr;
+    }
+    ListNode **indirect = &head;
+    ListNode *tail = head;
+    size_t listLength = 0;
+
+    // Chain the linked list to a circular linked list
+    while (tail->next) {
+      tail = tail->next;
+      ++listLength;
+    }
+    ++listLength; // Count the last node
+    tail->next = (*indirect);
+    indirect = &tail->next;
+
+    // Calculate rotation point and move to new head position:
+    size_t rotationOffset = listLength - k % listLength;
+    for (size_t pos = 0; pos < rotationOffset; ++pos) {
+      indirect = &(*indirect)->next;
+    }
+
+    ListNode *newHead = *indirect;
+    (*indirect) = nullptr;
+
+    return newHead;
+  }
+};
