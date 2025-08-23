85e36c0fabbab6ae4cf6fa54092abc5a414774ff
Yi-Ping Pan <pan.yiping.fi@gmail.com>
2025-08-23 20:09:32 +0800

    Add 746_Min_Cost_Climbing_Stairs cpp solution

---
+++ 746_Min_Cost_Climbing_Stairs/solution.cpp
@@ -0,0 +1,25 @@
+// Leetcode 746: Min Cost Climbing Stairs
+// @tag: dp, neetcode150
+// @difficulty: easy
+
+
+#include <vector>
+
+class Solution {
+public:
+  // Space complexity O(1)
+  // Time complexity O(n)
+  //
+  // Same idea as using foldl' in Haskell: we only need to keep track of the
+  // last two states. No need to keep the whole array.
+  int minCostClimbingStairs(std::vector<int> &cost) {
+    int prev1 = 0;
+    int prev2 = 0;
+    for (int idx = 0; idx < cost.size(); ++idx) {
+      int newPrev2 = cost[idx] + std::min(prev1, prev2);
+      prev1 = prev2;
+      prev2 = newPrev2;
+    }
+    return std::min(prev1, prev2);
+  }
+};
