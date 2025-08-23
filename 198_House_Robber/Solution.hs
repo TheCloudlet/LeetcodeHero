2d8c03797194e6cbd325ad2c8de059d58624b6a5
Yi-Ping Pan <pan.yiping.fi@gmail.com>
2025-08-23 22:28:32 +0800

    Add 198_House_Robber memo hs solution

---
+++ 198_House_Robber/Solution.hs
@@ -0,0 +1,40 @@
+-- Leetcode 198. House Robber
+
+module Solution where
+
+-- | Correct! But not efficient.
+-- `!!` operation is O(n), so in total this function is O(n^2).
+robMemo :: [Int] -> Int
+robMemo [] = 0
+robMemo [singleHouse] = singleHouse
+robMemo houses@(firstHouse : secondHouse : _) = last maxRobAtEachHouse
+  where
+    numHouses = length houses
+    maxRobAtEachHouse = map calculateMaxRobAtIndex [0 .. (numHouses - 1)]
+
+    calculateMaxRobAtIndex :: Int -> Int
+    calculateMaxRobAtIndex 0 = firstHouse
+    calculateMaxRobAtIndex 1 = max firstHouse secondHouse
+    calculateMaxRobAtIndex currentIndex =
+      let dontRobCurrent = maxRobAtEachHouse !! (currentIndex - 1)
+          robCurrent = houses !! currentIndex + maxRobAtEachHouse !! (currentIndex - 2)
+       in max dontRobCurrent robCurrent
+
+testHelper :: [Int] -> Int -> IO ()
+testHelper input expected =
+  if result == expected
+    then putStrLn $ "Test passed: " ++ show result
+    else putStrLn $ "Test failed: got " ++ show result ++ ", expected " ++ show expected
+  where
+    result = robMemo input
+
+main :: IO ()
+main = do
+  putStrLn "---------- Testing robMemo: ---------\n"
+  testHelper [1, 2, 3, 1] 4
+  testHelper [2, 7, 9, 3, 1] 12
+  testHelper [2, 1, 1, 2] 4
+  testHelper [1, 3, 1] 3
+  testHelper [1, 2] 2
+  testHelper [2] 2
+  testHelper [] 0
