fea6c88825b577d3802f8f057c18493d7fa9db91
Yi-Ping Pan <pan.yiping.fi@gmail.com>
2026-01-17 22:48:31 +0800

    feat: redo LC146 LRU (this is not easy)

--- problems/146_lru_cache/solution.cpp
+++ problems/146_lru_cache/solution.cpp
@@ -1,57 +1,60 @@
 // Leetcode 146. LRU Cache
 //
 // @tag: neetcode150
-// @difficulty: midium
+// @difficulty: medium
 //
 // Use doubly-linked list + hash map
 // Time: O(1) for both get and put
 
-
+#include <iterator>
 #include <list>
 #include <unordered_map>
 
 class LRUCache {
 private:
-  int capacity;
+  size_t capacity;
 
   // Doubly linked list: most recent at front, least recent at back
-  std::list<std::pair<int, int>> cache;
-
+  std::list<std::pair<int, int>> cache_list;
   // Hash map: key -> iterator to list node
-  std::unordered_map<int, std::list<std::pair<int, int>>::iterator> lookup;
+  std::unordered_map<int, std::list<std::pair<int, int>>::iterator> map;
 
 public:
-  LRUCache(int cap) : capacity(cap) {}
+  explicit LRUCache(int capacity_) : capacity(capacity_) {}
 
   int get(int key) {
-      auto it = lookup.find(key);
-      if (it == lookup.end()) {
-        return -1;
-      }
-      // Inefficient: Erasing and re-inserting recreates node
-      // Suggestion: use cache.splice(cache.begin(), cache, it->second) instead
-      auto node = *(it->second);
-      cache.erase(it->second);
-      cache.emplace_front(node);
-      lookup[key] = cache.begin();
-      return node.second;
+    if (auto it = map.find(key); it != map.end()) {
+      cache_list.splice(cache_list.begin(), cache_list, it->second);
+      return it->second->second;
+    }
+    return -1;
   }
 
   void put(int key, int value) {
-      if (lookup.count(key)) {
-        cache.erase(lookup[key]);
-      }
+    // In list already
+    if (auto it = map.find(key); it != map.end()) {
+      it->second->second = value;
+      cache_list.splice(cache_list.begin(), cache_list, it->second);
+      return;
+    }
 
-      // Insert to front
-      cache.emplace_front(key, value);
-      lookup[key] = cache.begin();
+    // Reuse the last element if full
+    if (cache_list.size() == capacity) {
+      auto last_it = std::prev(cache_list.end());
+      map.erase(last_it->first);
 
-      if (lookup.size() > capacity) {
-        //Note: Always pop from back (least recently used)
-        auto rm = cache.back();
-        lookup.erase(rm.first);
-        cache.pop_back();
-      }
+      last_it->first = key;
+      last_it->second = value;
+
+      cache_list.splice(cache_list.begin(), cache_list, last_it);
+
+      map.emplace(key, cache_list.begin());
+      return;
+    }
+
+    // Normal add
+    cache_list.emplace_front(key, value);
+    map.emplace(key, cache_list.begin());
   }
 };
 
