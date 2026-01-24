78089c8caafc5538755003b8493f7a9ca5553a77
Yi-Ping Pan <pan.yiping.fi@gmail.com>
2026-01-24 11:38:00 +0800

    refactor: redo and improve LC210

--- problems/210_course_schedule_ii/solution.cpp
+++ problems/210_course_schedule_ii/solution.cpp
@@ -2,52 +2,52 @@
 // @tag: topological-sort, bfs, graph
 // @difficulty: medium
 
-
 #include <queue>
 #include <vector>
 
 class Solution {
-public:
-  std::vector<int> findOrder(int numCourses,
-                             std::vector<std::vector<int>> &prerequisites) {
-
-    std::vector<std::vector<int>> adjVec(numCourses, std::vector<int>());
+ public:
+  std::vector<int> findOrder(
+      int numCourses, const std::vector<std::vector<int>>& prerequisites) {
+    // Use vector<vector<int>> not vector<list<int>> for cache locality
+    std::vector<std::vector<int>> adj_vec(numCourses);
     std::vector<int> indeg(numCourses, 0);
-    std::vector<int> result;
-    result.reserve(numCourses);
 
-    for (const auto &pair : prerequisites) {
-      int course = pair.at(0);
-      int prereqCourse = pair.at(1);
-      adjVec[prereqCourse].push_back(course);
-      ++indeg[course];
+    for (const auto& pair : prerequisites) {
+      const int to = pair.at(0);
+      const int from = pair.at(1);
+      adj_vec[from].push_back(to);
+      ++indeg[to];
     }
 
-    std::queue<int> waitToProcess;
+    std::queue<int> ready_queue;
 
     // Find all courses with zero in-degree
     for (int course = 0; course < numCourses; ++course) {
       if (indeg[course] == 0) {
-        waitToProcess.push(course);
+        ready_queue.push(course);
       }
     }
 
+    std::vector<int> result;
+    result.reserve(numCourses);
+
     // Remove courses with zero in-degree and update neighbors
-    while (!waitToProcess.empty()) {
-      int course = waitToProcess.front();
-      waitToProcess.pop();
-      result.push_back(course);
-      for (const auto &neighborCourse : adjVec[course]) {
-        if (--indeg[neighborCourse] == 0) {
-          waitToProcess.push(neighborCourse);
+    while (!ready_queue.empty()) {
+      int from = ready_queue.front();
+      ready_queue.pop();
+      result.push_back(from);
+
+      for (const auto& to : adj_vec[from]) {
+        if (--indeg[to] == 0) {
+          ready_queue.push(to);
         }
       }
-      adjVec[course].clear();
     }
 
     // There is a cycle
-    if (result.size() < numCourses) {
-      return {};
+    if (static_cast<int>(result.size()) < numCourses) {
+      return std::vector<int>{};
     }
 
     return result;
