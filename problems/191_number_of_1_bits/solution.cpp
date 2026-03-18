d78060d439b0d995b0d2b51d6dbc27fbe8c01f8b
Yi-Ping Pan <pan.yiping.fi@gmail.com>
2026-03-18 17:33:42 +0800

    feat: add LC191 Number of 1 Bits

---
+++ problems/191_number_of_1_bits/solution.cpp
@@ -0,0 +1,15 @@
+// Leecode 191. Number of 1 Bits
+
+class Solution {
+ public:
+  int hammingWeight(int n) { return __builtin_popcount(n); }
+
+  int hammingWeight2(int n) {
+    // Brian Kernighan Algorithm
+    int cnt = 0;
+    for (; n; ++cnt) {
+      n &= (n - 1);  // Clear the lowest bit
+    }
+    return cnt;
+  }
+};
