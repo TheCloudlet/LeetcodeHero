480afe684a0409cf589e84cdbac75ccb32101b79
Yi-Ping Pan <pan.yiping.fi@gmail.com>
2026-03-22 20:35:36 +0800

    feat: add LC137 Single Number II

---
+++ problems/137_single_number_ii/solution.cpp
@@ -0,0 +1,78 @@
+// Leetcode 137. Single Number II
+
+#if defined(BIT_MODULO)
+// This solution uses bitwise accumulation and modulo to separate the number
+// that appears once from those that appear three times.
+//
+// For each bit position (0 to 31), we count how many numbers have that bit set.
+// Since every number except the target appears three times, the bit count
+// modulo 3 will be 1 if the target has that bit set, otherwise 0.
+#include <numeric>
+#include <vector>
+
+class Solution {
+ public:
+  int singleNumber(const std::vector<int>& nums) {
+    int ans = 0;
+    for (int bit = 0; bit < 32; ++bit) {
+      // Count how many numbers have the current bit set.
+      // Use accumulate to sum the extracted bit (0 or 1) across all numbers.
+      int cnt =
+          std::accumulate(nums.begin(), nums.end(), 0, [bit](int sum, int num) {
+            return sum + ((static_cast<unsigned int>(num) >> bit) & 1);
+          });
+
+      // If the count modulo 3 equals 1, the target number has this bit set.
+      // So set the corresponding bit in the answer.
+      ans |= (cnt % 3) << bit;
+    }
+    return ans;
+  }
+};
+#endif
+
+#if defined(FSM)
+// Finite State Machine (FSM) approach: track bits modulo 3 using two integers.
+//
+// Each bit can be in one of three states:
+//   - 0: bit has appeared 0 times (mod 3)
+//   - 1: bit has appeared 1 time  (mod 3)
+//   - 2: bit has appeared 2 times (mod 3)
+//
+// We encode the state using two variables:
+//   ones: bits that are in state 1 (count ≡ 1)
+//   twos: bits that are in state 2 (count ≡ 2)
+//   (state 0 is when both are 0)
+///
+// Transition table for each incoming bit x (0 or 1):
+// +-----------+-----------+---+-----------+-----------+
+// | ones (in) | twos (in) | x | ones (out)| twos (out)|
+// +-----------+-----------+---+-----------+-----------+
+// |     0     |     0     | 0 |     0     |     0     |
+// |     0     |     0     | 1 |     1     |     0     |
+// |     1     |     0     | 0 |     1     |     0     |
+// |     1     |     0     | 1 |     0     |     1     |
+// |     0     |     1     | 0 |     0     |     1     |
+// |     0     |     1     | 1 |     0     |     0     |
+// +-----------+-----------+---+-----------+-----------+
+//
+// Derived logic:
+//   new_ones = (ones ^ x) & ~twos
+//   new_twos = (twos ^ x) & ~new_ones
+//
+// Applied bitwise to all 32 bits in parallel.
+#include <vector>
+
+class Solution {
+ public:
+  int singleNumber(const std::vector<int>& nums) {
+    int ones = 0;
+    int twos = 0;
+    for (int num: nums) {
+      ones = (ones ^ num) & ~twos;
+      twos = (twos ^ num) & ~ones;
+    }
+    return ones;
+  }
+};
+#endif
