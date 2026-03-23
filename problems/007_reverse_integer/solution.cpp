08d62261b9a72ca4f481d0adc749123f5d0f1f8b
Yi-Ping Pan <pan.yiping.fi@gmail.com>
2026-03-23 19:13:05 +0800

    feat(LC007): add recursive solution

--- problems/007_reverse_integer/solution.cpp
+++ problems/007_reverse_integer/solution.cpp
@@ -1,6 +1,10 @@
+// Leetcode 7. Reverse Integer
+
 #include <climits>
+
+#if defined(ITERATIVE)
 class Solution {
-public:
+ public:
   int reverse(int x) {
     int result = 0;
     int digit = 0;
@@ -16,3 +20,25 @@ public:
     return result;
   }
 };
+#endif
+
+#if defined(RECURSIVE)
+#include <climits>
+
+class Solution {
+ public:
+  int reverse(int x) {
+    return reverseHelper(x, 0);
+  }
+
+ private:
+  int reverseHelper(int x, int rev) {
+    if (x == 0) return rev;
+    int pop = x % 10;
+    if (rev > INT_MAX / 10 || (rev == INT_MAX / 10 && pop > 7)) return 0;
+    if (rev < INT_MIN / 10 || (rev == INT_MIN / 10 && pop < -8)) return 0;
+    rev = rev * 10 + pop;
+    return reverseHelper(x / 10, rev);
+  }
+};
+#endif
