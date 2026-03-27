1f3cc809a94a953f015f08c276179247a9d8c1cc
yipingp.us01 <pan.yiping.fi@gmail.com>
2026-03-27 10:22:24 +0800

    refactor(LC236): add comments

--- problems/236_lowest_common_ancestor_of_a_binary_tree/solution.cpp
+++ problems/236_lowest_common_ancestor_of_a_binary_tree/solution.cpp
@@ -1,5 +1,12 @@
 // Leetcode 236. Lowest Common Ancestor of a Binary Tree
-
+//
+// Two cases:
+// (1)   p    (2)  x
+//     /          / \
+//    q          p   q
+//
+// Key insight: p and q are guaranteed to exist in the tree.
+// If we hit p or q first, it must be the LCA (Case 1).
 class Solution {
  public:
   TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
@@ -10,7 +17,7 @@ class Solution {
     TreeNode* left = lowestCommonAncestor(root->left, p, q);
     TreeNode* right = lowestCommonAncestor(root->right, p, q);
 
-    if (left && right) {
+    if (left && right) {  // p and q found in different subtrees
       return root;
     }
 
