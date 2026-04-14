54d221500773565fee5b0af7d0b8c26fa7675a2c
Yi-Ping Pan <pan.yiping.fi@gmail.com>
2026-04-14 15:28:48 +0800

    refactor(LC239): refine solution and add comments

--- problems/239_sliding_window_maximum/solution.cpp
+++ problems/239_sliding_window_maximum/solution.cpp
@@ -9,27 +9,36 @@
 
 class Solution {
  public:
-  std::vector<int> maxSlidingWindow(std::vector<int>& nums, int k) {
-    assert(k <= nums.size());
-    std::deque<std::size_t> monoQueue;  // Non-increasing monotonic queue
-    std::vector<int> result;
-    result.reserve(nums.size() - k + 1);
+  std::vector<int> maxSlidingWindow(const std::vector<int>& nums, int k) {
+    assert(k >= 0);
 
-    for (std::size_t idx = 0; idx < nums.size(); ++idx) {
-      // Pop the outdated
-      while (!monoQueue.empty() && idx >= k && monoQueue.front() <= idx - k) {
-        monoQueue.pop_front();
-      }
-      // Remove smaller element from behind
-      while (!monoQueue.empty() && nums[monoQueue.back()] < nums[idx]) {
-        monoQueue.pop_back();
-      }
-      monoQueue.push_back(idx);
+    const int n = static_cast<int>(nums.size());
+    if (n < k) return {};
 
-      if (idx >= k - 1) {
-        result.push_back(nums[monoQueue.front()]);
+    constexpr int NINF = -1e9;
+
+    std::vector<int> ans(n - k + 1, NINF);
+    std::deque<int> dq;
+
+    for (int right = 0; right < n; ++right) {
+      const int left = right - k + 1;
+
+      // Insert right
+      // Remove elements from the back that are smaller than the current
+      // element. Since the current element is both larger and enters the window
+      // later, the older, smaller elements can never be the maximum. We can
+      // safely evict them.
+      while (!dq.empty() && nums[dq.back()] < nums[right]) {
+        dq.pop_back();
       }
+      dq.push_back(right);
+
+      // Remove outdated
+      if (dq.front() < left) dq.pop_front();
+
+      if (left >= 0) ans[left] = nums[dq.front()];  // CAUTION! We want value.
     }
-    return result;
+
+    return ans;
   }
 };
