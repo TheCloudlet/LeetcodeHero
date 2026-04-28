ca47d7165c29699d115d3e10ed225362237d8d18
Yi-Ping Pan <pan.yiping.fi@gmail.com>
2026-04-28 16:46:49 +0800

    feat(LC146): add vector version

--- problems/146_lru_cache/solution.cpp
+++ problems/146_lru_cache/solution.cpp
@@ -2,7 +2,8 @@
 //
 // @tag: neetcode150
 // @difficulty: medium
-//
+
+#if defined(LIST)
 // Use doubly-linked list + hash map
 // Time: O(1) for both get and put
 
@@ -58,9 +59,82 @@ class LRUCache {
   }
 };
 
-/**
- * Your LRUCache object will be instantiated and called as such:
- * LRUCache* obj = new LRUCache(capacity);
- * int param_1 = obj->get(key);
- * obj->put(key,value);
- */
+#endif
+
+#if defined(VECTOR)
+#include <unordered_map>
+#include <vector>
+
+class LRUCache {
+ private:
+  struct Node {
+    int key;
+    int value;
+    int next;
+    int prev;
+  };
+
+  std::vector<Node> nodes_;
+  std::unordered_map<int, int> key_to_idx_;
+  int front_ = -1;
+  int back_ = -1;
+  int free_idx_ = 0;
+  int cap_;
+
+  inline void detachLink(int i) {
+    if (nodes_[i].prev != -1)
+      nodes_[nodes_[i].prev].next = nodes_[i].next;
+    else
+      front_ = nodes_[i].next;
+    if (nodes_[i].next != -1)
+      nodes_[nodes_[i].next].prev = nodes_[i].prev;
+    else
+      back_ = nodes_[i].prev;
+  }
+
+  inline void addToFront(int i) {
+    nodes_[i].next = front_;
+    nodes_[i].prev = -1;
+    if (front_ != -1) nodes_[front_].prev = i;
+    front_ = i;
+    if (back_ == -1) back_ = i;
+  }
+
+ public:
+  explicit LRUCache(int capacity) : cap_(capacity) { nodes_.resize(capacity); }
+
+  int get(int key) {
+    auto it = key_to_idx_.find(key);
+    if (it == key_to_idx_.end()) return -1;
+
+    detachLink(it->second);
+    addToFront(it->second);
+
+    return nodes_[front_].value;
+  }
+
+  void put(int key, int value) {
+    auto it = key_to_idx_.find(key);
+    if (it != key_to_idx_.end()) {
+      nodes_[it->second].value = value;
+      detachLink(it->second);
+      addToFront(it->second);
+      return;
+    }
+
+    int reuse_idx = -1;
+    if (free_idx_ < cap_) {
+      reuse_idx = free_idx_++;
+    } else {
+      reuse_idx = back_;
+      key_to_idx_.erase(nodes_[reuse_idx].key);
+      detachLink(reuse_idx);
+    }
+
+    nodes_[reuse_idx].key = key;
+    nodes_[reuse_idx].value = value;
+    addToFront(reuse_idx);
+    key_to_idx_[key] = reuse_idx;
+  }
+};
+#endif
